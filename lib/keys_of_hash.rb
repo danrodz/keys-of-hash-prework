a60244ba9f7ec1a3693f15b208fb0f6cee3edff8
Daniel Rodriguez <danrodz15.dr@icloud.com>
2018-06-27 20:49:47 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,8 +2,8 @@ class Hash
   def keys_of(*arguments)
     # code goes here
     array = map do |k, v| 
-      if arguments.include?(value)  
-        key
+      if arguments.include?(v)  
+        k
       end
     end
     return array
